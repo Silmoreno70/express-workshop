52809678cc43ab0db0fcb6b2a918ccaf4875db4d
Silvestre Moreno Calderón <silmoreno70@gmail.com>
2020-10-12 19:00:24 -0500

    ADDED ternary operators, POST route and body Parser library

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const { pokemon } = require('./pokedex.json')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res, next) => {
     res.status(200)
     res.send('Bienvenido a la Pokedex')
 })
 
-app.get('/pokemon/all', (req, res, next) => {
+app.post('/pokemon', (req, res, next) => {
+    return res.status(200).send(req.body.name)
+})
+
+app.get('/pokemon', (req, res, next) => {
     let name = req.params.name
     res.status(200)
     res.send(pokemon)
@@ -17,23 +25,17 @@ app.get('/pokemon/all', (req, res, next) => {
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
     const id = req.params.id
     if (id >= 0 && id <= 150) {
-        res.status(200)
-        return res.send(pokemon[req.params.id - 1])
+        return res.status(200).send(pokemon[req.params.id - 1])
     }
-    res.status(404)
-    res.send('Pokemon no encontrado 😕')
+    return res.status(404).send('Pokemon no encontrado 😕')
 })
 
-app.get('/pokemon/:name', (req, res, next) => {
+app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
     const name = req.params.name
-    for (let i = 0; i < pokemon.length; i++) {
-        if (pokemon[i].name == name) {
-            res.status(200)
-            return res.send(pokemon[i])
-        }
-    }
-    res.status(404)
-    res.send('Pokemon no encontrado 😕')
+    const pk = pokemon.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) && p
+    })
+    return pk.length > 0 ? res.status(200).send(pk) : res.status(404).send('Pokemon no encontrado 😕')
 })
 
 app.listen(port, () => {
