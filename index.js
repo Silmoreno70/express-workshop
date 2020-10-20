56117c6901f04479a1e6f18316107f3f564dfbd9
Silvestre Moreno Calderón <silmoreno70@gmail.com>
2020-10-19 21:48:58 -0500

    REMOVED body-parser library CHANGED routes response

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const express = require('express')
 const app = express()
@@ -6,14 +5,20 @@ const port = process.env.PORT || 3000
 const pokemon = require('./routes/pokemon')
 
 app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res, next) => {
-    res.status(200)
-    res.send('Bienvenido a la Pokedex')
+    res.status(200).json({
+        code: 1,
+        message: 'Bienvenido a la Pokedex'
+    })
 })
 
 app.use('/pokemon', pokemon)
+app.use((req, res, next) => {
+    return res.status(404).json({ code: 404, message: 'URL no encontrada 😕' })
+})
 
 app.listen(port, () => {
     console.log('Server is running in port ' + port + '\nGo to http://localhost:' + port)
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,22 +8,37 @@ pokemon.post('/', (req, res, next) => {
 
 pokemon.get('/', async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon")
-    res.status(200).json(pkmn)
+    res.status(200).json({
+        code: 1,
+        message: pkmn
+    })
 })
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id
     if (id >= 0 && id <= 722) {
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = " + id)
-        return res.status(200).send(pkmn)
+        return res.status(200).json({
+            code: 1,
+            message: pkmn
+        })
     }
-    return res.status(404).send('Pokemon no encontrado 😕')
+    return res.status(404).json({
+        code: 1,
+        message: 'Pokemon no encontrado 😕'
+    })
 })
 
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const name = req.params.name
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name LIKE '" + name + "%'")
-    return pkmn.length > 0 ? res.status(200).send(pkmn) : res.status(404).send('Pokemon no encontrado 😕')
+    return pkmn.length > 0 ? res.status(200).json({
+        code: 1,
+        message: pkmn
+    }) : res.status(404).json({
+        code: 1,
+        message: 'Pokemon no encontrado 😕'
+    })
 })
 
 module.exports = pokemon
