78a4ed045960f92b38ba031540153e83acaa835d
Silvestre Moreno Calderón <silmoreno70@gmail.com>
2020-11-11 18:57:54 -0600

    ADDED- JWT implemented

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const morgan = require('morgan')
 const express = require('express')
 const app = express()
+// Selected port
 const port = process.env.PORT || 3000
+// Routers
 const pokemon = require('./routes/pokemon')
 const user = require('./routes/user')
+// Middleware
+const auth = require('./middleware/auth')
+const not_found = require('./middleware/notFound')
+const index = require('./middleware/index')
+const middleware = require('./middleware/index')
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        code: 1,
-        message: 'Bienvenido a la Pokedex'
-    })
-})
-
-app.use('/pokemon', pokemon)
+app.get('/', index)
 app.use('/user', user)
-app.use((req, res, next) => {
-    return res.status(404).json({ code: 404, message: 'URL no encontrada 😕' })
-})
+app.use(auth)
+app.use('/pokemon', pokemon)
+app.use(not_found)
 
 app.listen(port, () => {
     console.log('Server is running in port ' + port + '\nGo to http://localhost:' + port)
